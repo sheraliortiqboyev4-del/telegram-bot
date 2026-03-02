2c2c4e12bd9cfaed1294ae7a80b3a38cfa3f04d9
ortiqov_x7 <sheraliortiqboyev4@gmail.com>
2026-03-02 22:41:33 +0500

    fix: click

--- bot.js
+++ bot.js
@@ -2165,7 +2165,7 @@ async function startUserbot(client, chatId) {
                         
                         // Qat'iy tekshirish (Strict check)
                         // Faqat "olish", "клик" yoki "bosing" so'zlari bo'lsa bosadi
-                        if (btnText === 'olish' || btnText === 'клик' || btnText === '1💎  olish' || btnText === '1💎 olish' || btnText === '1🎁  olish' ) {
+                        if (btnText === 'olish' || btnText === 'клик' || btnText === '1💎  olish' || btnText === '1💎 olish' || btnText === '1🎁  olish' || btnText === 'Click'  ) {
                             console.log("[" + chatId + "] Tugma topildi (Strict): " + button.text);
                             try {
                                 await message.click(i, j);
